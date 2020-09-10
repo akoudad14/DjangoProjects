f00141058abf1c64f940954b5ce166be53e2690e
Marvin Akoudad <m.akoudad.perso@gmail.com>
2020-09-10 18:46:59 +0200

    Blog: seconde version - Ajout : email, commentaires, tags, recommandations

--- django_projects/blog/models.py
+++ django_projects/blog/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
 
 class PublishedManager(models.Manager):
 
@@ -28,6 +30,8 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
+    tags = TaggableManager()
+
     class Meta:
         ordering = ('-publish',)
 
@@ -36,3 +40,21 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+
+class Comment(models.Model):
+
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
